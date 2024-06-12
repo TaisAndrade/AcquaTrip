f9f90f79a117285f0eeb7898aea1e035f44529c0
Taís Andrade <102308855+TaisAndrade@users.noreply.github.com>
2024-06-12 09:30:23 -0300

    Update Conexao.java

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class Conexao {
     //Modificador que torna o método acessível fora da classe
     public Connection getConnection() throws SQLException{
-        //Declaração de variável local chamada "conexao" que armazena e retorna as informações de cxonexão com o Banco de Dados
-        //O nome do banco e a senha, foram removidos do projeto, todas as informações abaixo são padrões do posgresql
+        //Declaração de variável local chamada "conexao" que armazena e retorna as informações de conexão com o Banco de Dados
+        //O nome do banco e a senha, foram removidos do projeto, todas as informações abaixo são padrões do postgresql
         Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/nome_do_banco", "postgres", "senha_do_banco");
         return conexao;
     }
